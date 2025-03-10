c80b66b342192b9fc972cc17de35c969244a2c24
bryanjrr <bryanjoyarubio@gmail.com>
2025-03-10 19:18:47 +0100

    resumen cuestionario

--- src/components/resumen.tsx
+++ src/components/resumen.tsx
@@ -10,7 +10,7 @@ function Resumen() {
       <h2 className="text-2xl font-bold">Resumen de Respuestas</h2>
       <div className="bg-gray-100 p-4 rounded mt-4 text-left">
         {Object.entries(respuestas).map(([pregunta, respuesta]) => (
-          <p key={pregunta} className="mb-2">
+          <p key={pregunta} className="mb-2 text-black">
             <strong className="text-blue-600">{pregunta}:</strong> {respuesta}
           </p>
         ))}
